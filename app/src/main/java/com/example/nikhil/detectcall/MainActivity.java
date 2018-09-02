9d33ebc56ceb5bf7f594036b7dc3e03e140880e8
nikhil dhyani <nikhildhyani365@gmail.com>
2018-09-03 04:41:10 +0530

    Runtime Permission Added

--- app/src/main/java/com/example/nikhil/detectcall/MainActivity.java
+++ app/src/main/java/com/example/nikhil/detectcall/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.nikhil.detectcall;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
@@ -8,11 +9,20 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText sender, pass, receiverEmail;
+    TextView textView;
     Button btn;
     SharedPreferences sharedpreferences;
 
@@ -36,6 +46,55 @@ public class MainActivity extends AppCompatActivity {
         pass = findViewById(R.id.Email_Password);
         receiverEmail = findViewById(R.id.ReceiverEmail);
         btn = findViewById(R.id.Btn);
+
+        textView = findViewById(R.id.tv);
+
+
+        Dexter.withActivity(this)
+                .withPermission(Manifest.permission.READ_PHONE_STATE)
+                .withListener(new PermissionListener() {
+
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse response) {
+                        /* On Permission Granted */
+
+                        sender.setEnabled(true);
+                        pass.setEnabled(true);
+                        receiverEmail.setEnabled(true);
+                        btn.setEnabled(true);
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse response)
+                    {
+                        /*  Make Input Field Inaccessible*/
+                        sender.setEnabled(false);
+                        pass.setEnabled(false);
+                        receiverEmail.setEnabled(false);
+                        btn.setEnabled(false);
+
+                        textView.setText(" Please Grant Permission.\nEither Uninstall or Grant Permission From Settings");
+
+
+                    }
+
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token)
+                    {
+
+                        Toast.makeText(getApplicationContext(),"Need Read Phone State Permission To Detect Call",Toast.LENGTH_LONG).show();
+                        textView.setText(" Please Grant Permission.\nEither Uninstall or Grant Permission From Settings");
+
+                        sender.setEnabled(false);
+                        pass.setEnabled(false);
+                        receiverEmail.setEnabled(false);
+                        btn.setEnabled(false);
+
+
+                    }
+                }).check();
 
 
         //Code For Shared Preference
@@ -73,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                 editor.putString(senderE, senderEmail);
                 editor.putString(Passw, password);
                 editor.putString(receiverE, receiver);
-                emailx = receiver;
+
                 editor.commit();
 
                 Toast.makeText(MainActivity.this, "Details Saved In Your Mobile", Toast.LENGTH_LONG).show();
@@ -88,11 +147,11 @@ public class MainActivity extends AppCompatActivity {
     public void sendEmail(String number) {
 
         String subject = "Missed Call Alert";
-        String message = "Hey You have got a missed call from : "+number;
-        Log.d("TestEmail", emailx);
+        String message = "Hey You have got a missed call from : " + number;
+
 
         //Creating SendMail object
-        SendMail sm = new SendMail(this, emailx, subject, message,senderx,passx);
+        SendMail sm = new SendMail(this, emailx, subject, message, senderx, passx);
 
         //Executing sendmail to send email
         sm.execute();
